d0a6c330bfa4b4582d3381199bad504e027a07c0
Saans <92422813+saans808@users.noreply.github.com>
2022-07-02 14:35:48 +0900

    Javascript strings and more

---
+++ s15.js
@@ -0,0 +1,17 @@
+// 17
+let bestColor = "purple";
+let quote = 'You had me at "hello"';
+
+// 18
+const message = "    TASTE THE RAINBOW!  ";
+let whisper = message.trim().toLowerCase();
+
+// 19
+const word = "skateboard"; 
+let facialHair = word.slice(5,10).replace("o", "e");
+
+// 20
+const die1 = Math.floor(Math.random() * 6) + 1;
+const die2 = Math.floor(Math.random() * 6) + 1; 
+:
+const roll = `"You rolled a" + ${die1} + "and a " + ${die2} + ". They sum to " + ${die1+die2}`
