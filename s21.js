a8a53ebd5db58abd4db13688896c8444ed8b4543
Saans <92422813+saans808@users.noreply.github.com>
2022-07-29 22:43:26 +0900

    Leveling up our functions

---
+++ s21.js
@@ -0,0 +1,15 @@
+// 44
+let square = function(num) {
+  //  return num * num;
+    return Math.pow(num, 2);
+}
+
+// 45
+const square = {
+    area(side) {
+        return side * side;
+    },
+    perimeter(side) {
+        return side * 4;
+    }
+}
